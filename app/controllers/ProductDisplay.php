1033a5246146a7b70289521b543dae9b05d0613e
hxndani <142963467+hxndani@users.noreply.github.com>
2024-10-23 20:26:42 +0700

    Update ProductDisplay.php

--- app/controllers/ProductDisplay.php
+++ app/controllers/ProductDisplay.php
@@ -3,13 +3,10 @@
 class ProductDisplay extends BaseController
 {
 
-    public function showAllProducts($f3)
-    {
+    public function showAllProducts($f3) {
         $_products = new _product_display($this->db);
-
-        // try {
         $get = $_products->getAllProducts();
-
+    
         $data = array();
         foreach ($get as $item) {
             $parse = array(
@@ -18,48 +15,46 @@ class ProductDisplay extends BaseController
                 'price' => $item['price'],
                 'image' => $item['image'],
                 'motor' => $item['motor'],
-                'sold' => $item['stock_out']
+                'sold' => $item['stock_out'],
+                'promo_price' => $item['promo_price'], // Tambahkan promo_price
+                'discount_percent' => $item['discount_percent'], // Tambahkan discount_percent
+                'start_date' => $item['start_date'],
+                'end_date' => $item['end_date']
+
             );
             array_push($data, $parse);
         }
-
+    
         header('Content-Type: application/json');
         echo json_encode($data);
-        // } catch (Exception $e) {
-        //     error_log($e->getMessage());
-        //     header("HTTP/1.1 500 Internal Server Error");
-        //     echo json_encode(["error" => "Internal Server Error"]);
-        // }
     }
+    
 
-    public function showDetailProducts($f3, $params)
-    {
+    public function showDetailProducts($f3, $params) {
         $id = $params['id'];
         $_products = new _product_display($this->db);
-
-        // try {
         $get = $_products->getDetailProduct($id);
-
-        $data = array();
-        foreach ($get as $item) {
-            $parse = array(
-                'id' => $item['id'],
-                'name' => $item['name'],
-                'price' => $item['price'],
-                'image' => $item['image'],
-                'motor' => $item['motor'],
-                'stock' => $item['stock_in'] - $item['stock_out'],
-                'sold' => $item['stock_out']
-            );
-            array_push($data, $parse);
+    
+        if ($get) {
+            $product = $get[0];  // Hanya satu produk berdasarkan ID
+            $data = [
+                'id' => $product['id'],
+                'name' => $product['name'],
+                'price' => $product['price'],
+                'final_price' => $product['final_price'],  // Harga promo atau harga asli
+                'image' => $product['image'],
+                'motor' => $product['motor'],
+                'stock' => $product['stock_in'] - $product['stock_out'],
+                'sold' => $product['stock_out']
+            ];
+    
+            header('Content-Type: application/json');
+            echo json_encode($data);
+        } else {
+            header("HTTP/1.1 404 Not Found");
+            echo json_encode(["error" => "Product not found"]);
         }
-
-        header('Content-Type: application/json');
-        echo json_encode($data);
-        // } catch (Exception $e) {
-        // error_log($e->getMessage());
-        // header("HTTP/1.1 500 Internal Server Error");
-        // echo json_encode(["error" => "Internal Server Error"]);
-        // }
     }
+    
+    
 }
