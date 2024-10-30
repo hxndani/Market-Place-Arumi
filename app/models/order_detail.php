8c1f01a70d033cfde683e27e80c8177aa87aa0e7
hxndani <142963467+hxndani@users.noreply.github.com>
2024-10-30 11:01:18 +0700

    Update order_detail.php

--- app/models/order_detail.php
+++ app/models/order_detail.php
@@ -2,12 +2,21 @@
 
 class Order_detail extends DB\SQL\Mapper {
     public function __construct(DB\SQL $db) {
-        parent::__construct($db, 'order_detail');
+        parent::__construct($db, 'order_items');
     }
 
     function getById($id){
         $this->load(array('id=?',$id));
     }
 
+    public function order_detail($order_id, $product_id, $quantity, $price, $subtotal){
+        $this->order_id = $order_id;
+        $this->quantity = $quantity;
+        $this->price_at_purchase = $price;
+        $this->subtotal = $subtotal;
+        $this->product_id = $product_id;
+        $this->save();  
+
+    }
 
 }
